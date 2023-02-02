ccbcf4a0d780f42d6c39b6b7eb0c5752cc5a12f2
minjko <komj36@gmail.com>
2023-02-02 17:02:14 +0900

    develop application - 3(3). run main

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -8,5 +9,9 @@ public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 인수 : persistence-unit의 name 값(persistence.xml -> hello);
+        EntityManager em = emf.createEntityManager();
+
+        em.close();
+        emf.close();
     }
 }
